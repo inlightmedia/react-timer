7ef91093e19cda179c841abb6f7c1ebba8f26869
Joshua Dyck <josh.dyck@inlightmedia.ca>
2016-06-07 17:24:32 -0400

    Add countdown functionality

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,40 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+	it('should exist', () => {
+		expect(Countdown).toExist();
+	});
+
+	describe('handleSetCountdown', () => {
+		it('should set state to started and begin countdown', (done) => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown />);
+			countdown.handleSetCountdown(10);
+			expect(countdown.state.count).toBe(10);
+			expect(countdown.state.countdownStatus).toBe('started');
+			setTimeout(()=>{
+				expect(countdown.state.count).toBe(9);
+				done();
+			}, 1010);
+
+
+		});
+
+		it('should never be negative', (done) => { // pass in done so that the test will not finish until async code completes 
+			var countdown = TestUtils.renderIntoDocument(<Countdown />);
+			countdown.handleSetCountdown(1);
+			expect(countdown.state.count).toBe(1);
+			expect(countdown.state.countdownStatus).toBe('started');
+			setTimeout(()=>{
+				expect(countdown.state.count).toBe(0);
+				done();
+			}, 3010);
+		});
+	});
+
+});
